f61f01e32548ea5706ae96986e184b4dcc9d0817
frkylmz <farukyilmaz420@gmail.com>
2022-04-26 18:00:16 +0300

    add props lesson

--- 02-props/src/App.js
+++ 02-props/src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+
+import User from './components/User' 
+
+const friends = [
+  {
+    id: 1,
+    name: "Ahmet",
+  },
+  {
+    id: 2,
+    name: "Tayfun",
+  },
+  {
+    id: 3,
+    name: "Gökalp",
+  },
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <User 
+        // name="Faruk" 
+        surname="Yılmaz" 
+        isLoggedIn={true} 
+        age={"23"} 
+        friends={friends}
+        address={{
+          title: "Bornova/İzmir",
+          zip: 34755
+        }} 
+      />
+    </>
   );
 }
 
