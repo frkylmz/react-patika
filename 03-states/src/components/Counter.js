a986bb3877679bbeb3df0c7dd6f17480e204d032
frkylmz <farukyilmaz420@gmail.com>
2022-04-27 17:34:19 +0300

    add states lesson

---
+++ 03-states/src/components/Counter.js
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+
+function Counter() {
+
+  const[count, setCount] = useState(0);
+  
+  const decrease = () => {
+    setCount(count -1);
+  }
+  
+  const increase = () => {
+      setCount(count +1);
+  }
+
+  const reset = () => {
+      setCount(0);
+  }
+  
+  return (
+    <div>
+        <h1>{count}</h1>
+        <button onClick={decrease}>Decrease</button>
+        <button onClick={increase}>Increase</button>
+        <button onClick={reset}>Reset</button>
+
+        
+    </div>
+  )
+}
+
+export default Counter
