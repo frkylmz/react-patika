25ac53141b82d13480181159a4c32bb17b133db1
frkylmz <farukyilmaz420@gmail.com>
2022-06-27 20:41:53 +0300

    add NavLink lesson

--- 07-routing/src/components/Users.js
+++ 07-routing/src/components/Users.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { Link, Switch, Route, useRouteMatch } from "react-router-dom";
+import { NavLink, Switch, Route, useRouteMatch } from "react-router-dom";
 import axios from "axios";
 
 import User from "./User";
@@ -24,7 +24,9 @@ function Users() {
       <ul>
         {users.map((user) => (
           <li key={user.id}>
-            <Link to={`${url}/${user.id}`}>{user.name}</Link>
+            <NavLink activeClassName="active" to={`${url}/${user.id}`}>
+              {user.name}
+            </NavLink>
           </li>
         ))}
       </ul>
