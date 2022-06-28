fbd12bc0dd6325b41f9701bb586fdd35f65398c5
frkylmz <farukyilmaz420@gmail.com>
2022-06-28 14:41:24 +0300

    add Form Validation 2 lesson

--- 08-formik/src/components/validation.js
+++ 08-formik/src/components/validation.js
@@ -2,11 +2,13 @@ import { object, string } from "yup";
 import * as yup from "yup";
 
 const validations = object({
-  email: string().email().required(),
-  password: string().min(5).required(),
+  email: string().email("Geçerli bir email girin.").required("Zorunlu alan."),
+  password: string()
+    .min(5, "Parolanız en az 5 karakter olmalıdır.")
+    .required("Zorunlu alan."),
   passwordConfirm: string()
-    .oneOf([yup.ref("password")])
-    .required(),
+    .oneOf([yup.ref("password")], "Parolalar uyuşmuyor.")
+    .required("Zorunlu alan."),
 });
 
 export default validations;
