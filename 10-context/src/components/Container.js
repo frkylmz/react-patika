5423f734dc60f772988055d76baee9a914072da5
frkylmz <farukyilmaz420@gmail.com>
2022-06-28 21:38:07 +0300

    add Multi Context lesson

--- 10-context/src/components/Container.js
+++ 10-context/src/components/Container.js
@@ -4,15 +4,19 @@ import Button from "./Button";
 import Header from "./Header";
 
 import ThemeContext from "../context/ThemeContext";
+import Profile from "./Profile";
 
 function Container() {
   const { theme } = useContext(ThemeContext);
+
   return (
     <div className={`app ${theme}`}>
       <Header />
-
       <hr />
       <Button />
+
+      <hr />
+      <Profile />
     </div>
   );
 }
