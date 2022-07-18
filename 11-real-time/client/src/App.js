15d796bda1154cfe75b151a0bbf080e7af9630f7
frkylmz <farukyilmaz420@gmail.com>
2022-07-18 15:27:33 +0300

    add socket server lesson

---
+++ 11-real-time/client/src/App.js
@@ -0,0 +1,19 @@
+import "./App.css";
+
+import { useEffect } from "react";
+import { init } from "./socketApi";
+import Palette from "./components/Palette";
+
+function App() {
+  useEffect(() => {
+    init();
+  }, []);
+
+  return (
+    <div className="App">
+      <Palette />
+    </div>
+  );
+}
+
+export default App;
